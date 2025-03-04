2b234a52a0290fbf47fbdf2dfc940f380836dd68
vonguyenvu98 <vonguyenvu@bytesquad.tech>
2025-03-04 15:27:48 +0700

    update chat v2

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -15,32 +15,40 @@ export default function LoginPage() {
 
     return (
         <LoginGuard>
-            <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className=" flex items-center justify-center h-screen bg-gray-100">
                 <form
                     onSubmit={handleLogin}
-                    className="p-6 bg-white shadow-lg rounded-lg"
+                    className="p-6 bg-[#6a5548] shadow-lg rounded-lg w-[30%]"
                 >
-                    <h2 className="text-2xl font-bold mb-4">Login</h2>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        className="w-full p-2 border mb-2"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        className="w-full p-2 border mb-4"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-500 text-white py-2"
-                    >
-                        Login
-                    </button>
+                    <h2 className="text-2xl font-bold mb-4">Sign In</h2>
+                    <div className="flex justify-between items-center">
+                        <span>Username</span>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className=" p-2  mb-2 text-black rounded w-[70%] bg-white"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-between items-center ">
+                        <span>Password</span>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            className=" p-2  mb-4 text-black rounded w-[70%] bg-white"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <button
+                            type="submit"
+                            className="w-[40%]  bg-orange-500 text-white py-2"
+                        >
+                            Login
+                        </button>
+                    </div>
                 </form>
             </div>
         </LoginGuard>
